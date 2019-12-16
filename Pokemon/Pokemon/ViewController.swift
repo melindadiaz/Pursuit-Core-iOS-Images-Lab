10ce3dafe1166c58f614c143653447234f33e479
melinda <melindad_82@msn.com>
2019-12-16 14:04:57 -0500

    Started plan on project

---
+++ Pokemon/Pokemon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokemon
+//
+//  Created by Melinda Diaz on 12/16/19.
+//  Copyright © 2019 Melinda Diaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+//start
+}
+
