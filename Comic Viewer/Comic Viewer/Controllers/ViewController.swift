ab4aab0e1d120cdb3ad4f9a993a5eb5d7e189a13
melinda <melindad_82@msn.com>
2020-01-05 08:17:33 -0500

    Set UI in storyboard

---
+++ Comic Viewer/Comic Viewer/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Comic Viewer
+//
+//  Created by Melinda Diaz on 1/5/20.
+//  Copyright © 2020 Melinda Diaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     
+    }
+
+
+}
+
---
+++ Comic Viewer/Comic Viewer/Model.swift
@@ -0,0 +1,12 @@
+//
+//  Model.swift
+//  Comic Viewer
+//
+//  Created by Melinda Diaz on 1/5/20.
+//  Copyright © 2020 Melinda Diaz. All rights reserved.
+//
+
+import Foundation
+
+
+
